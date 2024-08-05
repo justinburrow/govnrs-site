788d8f02c7a7b536503e0f20f9a817652a2fe291
Justin Burrow <justinburrow@gmail.com>
2024-08-05 16:39:15 -0700

    added daisyui and mobile nav panel layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        vazirmatn: ['Vazirmatn', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [daisyui],
+};
